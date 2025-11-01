11d1daade5a0e34b466bde4651744dda93aedca1
dantecsm <4spadenil@gmail.com>
2025-11-01 22:33:57 +0800

    feat: Add EDC calcution

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const fs = require('fs')
+const { getEDC } = require('./edc')
 const { getECC } = require('./ecc')
 
 // 用法示例
 const buffer = fs.readFileSync('sector.bin')
+const edcBuffer = getEDC(buffer)
 const eccBuffer = getECC(buffer)
 
-// 校验
+// 校验 EDC 结果
+console.log(edcBuffer)
+const edcGT = buffer.subarray(16 + 2048, 16 + 2048 + 4)  // E5 FA 31 CB
+isEqual = Buffer.compare(edcBuffer, edcGT) === 0
+console.log(isEqual ? 'EDC 正确' : 'EDC 错误')
+
+// 校验 ECC 结果
 console.log(eccBuffer)
 const eccGT = buffer.subarray(16 + 2048 + 4 + 8)
 isEqual = Buffer.compare(eccBuffer, eccGT) === 0
